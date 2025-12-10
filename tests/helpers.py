4b343b3b55b63fdbe28bb32fe0595ec2c2af8678
Charlotte Gale <CharlotteMGale@outlook.com>
2025-12-10 10:52:59 +0000

    removes comment

--- tests/helpers.py
+++ tests/helpers.py
@@ -4,6 +4,5 @@ def generate_contents(word_count):
     return " ".join(["word"] * word_count)
 
 def generate_uk_mobile():
-    # Generate 9 random digits after the '07'
     number = '07' + ''.join([str(random.randint(0, 9)) for _ in range(9)])
     return number
